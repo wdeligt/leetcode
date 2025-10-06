5e03049d01cf3cc2c85b8923a18fea9aad369a59
Wouter de Ligt <150262066+wdeligt@users.noreply.github.com>
2025-10-06 09:15:26 +0000

    Sync LeetCode submission Runtime - 7 ms (81.17%), Memory - 18.4 MB (100.00%)

---
+++ 0125-valid-palindrome/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = "".join(char for char in s if char.isalnum()).lower()
+        l = 0 
+        r = len(string) - 1
+        while l < r:
+            if string[l] != string[r]:
+                return False
+            l += 1 
+            r -= 1 
+        
+        return True
