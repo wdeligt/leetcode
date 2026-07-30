516dc5a0c670e3448cef798a6f14cad99e1853bc
Wouter de Ligt <150262066+wdeligt@users.noreply.github.com>
2026-07-30 12:01:44 +0000

    Sync LeetCode submission Runtime - 1399 ms (74.32%), Memory - 0.0B (100.00%)

---
+++ 0607-sales-person/solution.sql
@@ -0,0 +1,11 @@
+WITH red AS (
+    SELECT DISTINCT o.sales_id
+    FROM Orders o 
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name ="RED"
+)
+
+SELECT DISTINCT name
+FROM SalesPerson  
+WHERE sales_id NOT IN (SELECT sales_id FROM red)
+
