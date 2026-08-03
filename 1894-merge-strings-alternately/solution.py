2cab5b778116230c9f23455a42954feab97bc999
Wouter de Ligt <150262066+wdeligt@users.noreply.github.com>
2026-08-03 17:37:23 +0000

    Sync LeetCode submission Runtime - 48 ms (37.57%), Memory - 19.2 MB (88.38%)

--- 1894-merge-strings-alternately/solution.py
+++ 1894-merge-strings-alternately/solution.py
@@ -1,18 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        answer = ""
-        n = len(word1)
-        m = len(word2)
-        if n == m:
-            for i in range(n):
-                answer += word1[i] + word2[i]
-        elif n < m:
-            for j in range(n):
-                answer += word1[j] + word2[j]
-            answer += word2[j+1:]
-        elif m < n:
-            for k in range(m):
-                answer += word1[k] + word2[k]
-            answer += word1[k+1:]      
-        return answer
-
+        result = []
+        i = 0 
+        while i < len(word1) and i < len(word2):
+            result.append(word1[i])
+            result.append(word2[i])
+            i += 1
+        if len(word1) < len(word2):
+            return ''.join(result) + word2[i:]
+        else:
+            return ''.join(result) + word1[i:]
