16a1c4b9643d24ecfaa5be4abac703bd6ed7ac2f
Wouter de Ligt <150262066+wdeligt@users.noreply.github.com>
2026-08-08 11:37:48 +0000

    Sync LeetCode submission Runtime - 561 ms (18.65%), Memory - 0.0B (100.00%)

---
+++ 3803-find-products-with-valid-serial-numbers/solution.sql
@@ -0,0 +1,8 @@
+SELECT product_id, product_name, description
+FROM products
+WHERE REGEXP_LIKE(
+    description,
+    '(^|[^A-Za-z0-9])SN[0-9]{4}-[0-9]{4}([^A-Za-z0-9]|$)',
+    'c'
+)
+ORDER BY product_id;
